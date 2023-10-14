190c468116fca04761fc2b6295f5ea8abbfa9985
j22-robertson <55785515+j22-robertson@users.noreply.github.com>
2023-10-14 14:38:26 +0100

    Created a window to render in using Windows API
    Added window initialization functions into stdafx.h
    - WndProc
    - mainloop
    - InitializeWindow

--- DissertationRaytracerDirectX12/main.cpp
+++ DissertationRaytracerDirectX12/main.cpp
@@ -4,5 +4,21 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
 
+
+
+
+	bool FullScreen =false;
+
+	if (!InitializeWindow(hInstance, nCmdShow, Width, Height,FullScreen))
+	{
+		MessageBox(0, L"Window Initializaiton - Failed", L"Error", MB_OK);
+		return 0;
+	}
+	
+	mainloop();
+	
+
 	return 0;
 }
+
+
--- DissertationRaytracerDirectX12/stdafx.h
+++ DissertationRaytracerDirectX12/stdafx.h
@@ -10,3 +10,135 @@
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
 #include "d3dx12.h"
+
+//Window Handle
+HWND hwnd = NULL;
+
+
+//Window name
+LPCTSTR WindowName = L"Raytracer";
+
+//Window title
+
+LPCTSTR WindowTitle = L"Raytracer";
+
+int Width = 800;
+int Height = 800;
+
+
+void mainloop()
+{
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+
+
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//Update loop goes here
+		}
+	}
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) {
+			if (MessageBox(0, L"Are you sure you wnat to exit?", L"Confirm, do you want to exit program?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+				DestroyWindow(hWnd);
+		}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+
+}
+
+
+
+
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool fullscreen)
+{
+	if (fullscreen)
+	{
+		HMONITOR hmon = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+		MONITORINFO mi = { sizeof(mi) };
+
+		GetMonitorInfo(hmon, &mi);
+
+		width = mi.rcMonitor.right - mi.rcMonitor.left;
+
+		height = mi.rcMonitor.bottom - mi.rcMonitor.top;
+
+	}
+
+		//Window structure setup
+		WNDCLASSEX wc;
+
+		wc.cbSize = sizeof(WNDCLASSEX);
+		wc.style = CS_HREDRAW | CS_VREDRAW;
+		wc.lpfnWndProc = WndProc;
+		wc.cbClsExtra = NULL;
+		wc.cbWndExtra = NULL;
+		wc.hInstance = hInstance;
+		wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+		wc.lpszMenuName = NULL;
+		wc.lpszClassName = WindowName;
+		wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+
+
+	
+
+		if (!RegisterClassEx(&wc))
+		{
+			MessageBox(NULL, L"Error registering class", L"Error", MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+
+		hwnd = CreateWindowEx(NULL,
+			WindowName,
+			WindowTitle,
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT, CW_USEDEFAULT,
+			width, height,
+			NULL,
+			NULL,
+			hInstance,
+			NULL);
+
+		if (!hwnd)
+		{
+			MessageBox(NULL, L"Error creating window", L"Error", MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (fullscreen)
+		{
+			SetWindowLong(hwnd, GWL_STYLE, 0);
+		}
+
+		ShowWindow(hwnd, ShowWnd);
+		UpdateWindow(hwnd);
+
+		return true;
+
+	
+}
+
