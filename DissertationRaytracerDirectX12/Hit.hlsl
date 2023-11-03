37aee2c6d0b75e9447c7e2f2cfca191ca810ebce
j22-robertson <55785515+j22-robertson@users.noreply.github.com>
2023-11-03 11:41:32 +0000

    Update Hit.hlsl

--- DissertationRaytracerDirectX12/Hit.hlsl
+++ DissertationRaytracerDirectX12/Hit.hlsl
@@ -44,6 +44,8 @@ void ClosestHit(inout HitInfo payload, Attributes attrib)
 void PlaneClosestHit(inout HitInfo payload, Attributes attrib)
 {
     float3 barycentrics = float3(1.f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
+    
     float3 hitColor = float3(0.7, 0.7, 0.3);
+
     payload.colorAndDistance = float4(hitColor, RayTCurrent());
 }
